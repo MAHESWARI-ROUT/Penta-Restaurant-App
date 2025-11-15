cb6934313269d94979b0116312ac21971f0073ec
dev <147903617+chandan-droid@users.noreply.github.com>
2025-11-15 21:18:02 +0530

    Changes made

--- lib/commons/app_constants.dart
+++ lib/commons/app_constants.dart
@@ -1,5 +1,4 @@
 class AppConstants {
   static const String appName = 'OTDC Ordering System';
-  // Centralized base URL for network requests
   static const String baseUrl = 'https://otdc.lasolution.in/';
 }
